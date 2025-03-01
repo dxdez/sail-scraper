8808c31db300a1162289206397ecd49d86b4ff53
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-03-01 08:48:30 -0500

    test: integrate basic variable into html template with random number

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html') 
+    random_num = random.randint(1, 100)
+    return render_template('index.html', random_number=random_num) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
