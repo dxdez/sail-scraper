7d7b309b3568ab6672dbbe6b4578cf21010be5c8
dylanxhernandez <dylan.x.h.developer@gmail.com>
2025-03-03 17:59:31 -0500

    feat: add basic retrieval from page

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
-import random
+import requests
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    random_num = random.randint(1, 100)
-    return render_template('index.html', random_number=random_num) 
+    return render_template('index.html') 
+
+@app.route('/scrape', methods=['POST'])
+def execute_scrape():
+    url = request.form.get('url')
+    response = requests.get(url)
+    supersoup = BeautifulSoup(response.text, 'html.parser')
+
+    text_display = supersoup.get_text()   
+    return render_template('index.html', result=text_display) 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
